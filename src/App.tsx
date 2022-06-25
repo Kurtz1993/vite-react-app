ad516d13f485d4066df480cc7e58a2f5c8f34fee
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-06-25 13:09:58 -0500

    Switch styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,36 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import logo from './logo.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="bg-slate-600 text-center flex flex-col items-center justify-center h-screen text-white">
+      <img src={logo} className="w-64" alt="logo" />
+      <p>Hello Vite + React!</p>
+      <p>
+        <button
+          type="button"
+          onClick={() => setCount(count => count + 1)}
+          className="rounded bg-teal-700 text-white py-1 p-4 transition duration-200 shadow-md hover:bg-teal-800 hover:shadow-lg"
+        >
+          count is: {count}
+        </button>
+      </p>
+      <p>
+        Edit <code>App.tsx</code> and save to test HMR updates.
+      </p>
+      <p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+        {' | '}
+        <a className="App-link" href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener noreferrer">
+          Vite Docs
+        </a>
+      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
