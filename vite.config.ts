71b0b0283cdd79edd09154cdd5f50136733f0e65
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2022-06-30 18:36:46 -0500

    Add plugin checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,20 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), svgr()],
+  plugins: [tsconfigPaths(), react(), checker({ typescript: true }), svgr()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
     coverage: {
       reporter: ['text', 'html'],
-      exclude: [
-        'node_modules/',
-        'src/setupTests.ts',
-      ],
+      exclude: ['node_modules/', 'src/setupTests.ts'],
     },
   },
-})
+});
