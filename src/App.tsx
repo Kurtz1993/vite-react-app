4d99e73a2597f559da2c3a54a8bba7745d793638
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2023-01-16 11:24:56 -0600

    Adjust linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import HelloWorld from '@app/components/HelloWorld';
 import { useState } from 'react';
+
+import HelloWorld from '@app/components/HelloWorld';
+
 import { ReactComponent as Logo } from './logo.svg';
 
 function App() {
@@ -9,16 +11,20 @@ function App() {
     <div className="text-center">
       <header className="bg-gray-900 text-white flex flex-col items-center justify-center min-h-screen text-xl">
         <Logo className="h-[40vmin] animate-bounce" />
-        <p className='my-4'>Hello Vite + React!</p>
-        <p className='my-4'>
-          <button type="button" onClick={() => setCount(count => count + 1)} className="bg-cyan-800 text-white py-2 px-6 rounded">
+        <p className="my-4">Hello Vite + React!</p>
+        <p className="my-4">
+          <button
+            type="button"
+            onClick={() => setCount(prev => prev + 1)}
+            className="bg-cyan-800 text-white py-2 px-6 rounded"
+          >
             count is: {count}
           </button>
         </p>
-        <p className='my-4'>
+        <p className="my-4">
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
-        <p className='my-4'>
+        <p className="my-4">
           <a className="text-cyan-300" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
